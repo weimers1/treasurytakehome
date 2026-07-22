cabebfc17cea6b42b1645e7fb1834cda0063cd3c
Sam Weimer <samweimer7@gmail.com>
2026-07-22 04:12:16 +0000

    feat: add Docker and cloud-build instructions

--- alcohol-label-scanner/next.config.ts
+++ alcohol-label-scanner/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   allowedDevOrigins: ['3000-cs-964002712623-default.cs-us-east1-vpcf.cloudshell.dev'],
 };
 
